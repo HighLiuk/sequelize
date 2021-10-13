1c60ff1a603d1e269ccfbb9d384ddd99ca096532
Luca Di Fazio <highliuk@gmail.com>
2021-10-13 18:27:03 +0200

    Add POST /users

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const { sequelize } = require("./models/index")
+const { sequelize, User } = require("./models/index")
 const express = require("express")
 const app = express()
+
+app.use(express.json())
+
+app.post("/users", async (req, res) => {
+  const { name, email, role } = req.body
+
+  try {
+    const user = await User.create({ name, email, role })
+    return res.json(user)
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json(err)
+  }
+})
 
 app.listen(5000, async () => {
   console.log("Server up on http://localhost:5000")
