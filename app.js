33592ebf78e8f9064380c12fd075d0dd68ec5048
Luca Di Fazio <highliuk@gmail.com>
2021-10-14 09:24:46 +0200

    Add Post Routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const { sequelize } = require("./models/index")
 const express = require("express")
 const userRouter = require("./routes/users")
+const postRouter = require("./routes/posts")
 
 const app = express()
 
 app.use(express.json())
 app.use("/users", userRouter)
+app.use("/posts", postRouter)
 
 app.listen(5000, async () => {
   console.log("Server up on http://localhost:5000")
---
+++ routes/posts.js
@@ -0,0 +1,5 @@
+const { Post } = require("../models/index")
+const express = require("express")
+const app = express.Router()
+
+module.exports = app
