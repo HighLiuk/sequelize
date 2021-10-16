c83853962a4e704d23980119318a81f03f35ea94
Luca Di Fazio <highliuk@gmail.com>
2021-10-16 19:43:51 +0200

    Added CRUD /posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -12,7 +12,37 @@ app.post("/", (req, res, next) => {
 
 app.get("/", (req, res, next) => {
   Post.findAll({ include: "user" })
-    .then((posts) => res.json({ posts }))
+    .then((posts) => res.status(200).json({ posts }))
+    .catch(next)
+})
+
+app.get("/:uuid", (req, res, next) => {
+  const { uuid } = req.params
+  Post.findOne({ where: { uuid }, include: "user", rejectOnEmpty: true })
+    .then((post) => res.status(200).json(post))
+    .catch(next)
+})
+
+app.put("/:uuid", (req, res, next) => {
+  const { uuid } = req.params
+  const { body } = req.body
+  Post.findOne({ where: { uuid }, rejectOnEmpty: true })
+    .then((post) => {
+      post.update({ body })
+      return post
+    })
+    .then((post) => res.status(201).json(post))
+    .catch(next)
+})
+
+app.delete("/:uuid", (req, res, next) => {
+  const { uuid } = req.params
+  Post.findOne({ where: { uuid }, rejectOnEmpty: true })
+    .then((post) => {
+      post.destroy()
+      return post
+    })
+    .then((post) => res.status(200).json(post))
     .catch(next)
 })
 
